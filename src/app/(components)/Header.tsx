a4d9bbf7e5018dd1d93d7315ff4b4488fb2a8f5f
Ger4eK <ger4ek228@gmail.com>
2023-06-19 22:27:22 +0300

    logo fix

--- src/app/(components)/Header.tsx
+++ src/app/(components)/Header.tsx
@@ -1,174 +1,173 @@
-"use client";
+'use client';
 
-import { Fragment, useState } from "react";
-import { Dialog, Transition } from "@headlessui/react";
-import { SvgBurger, SvgCareLogo, SvgCart, SvgLogo } from "../(svg)/AllSvg";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import classNames from "classnames";
+import { Fragment, useState } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import { SvgBurger, SvgCareLogo, SvgCart, SvgLogo } from '../(svg)/AllSvg';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import classNames from 'classnames';
 
 export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const pathname = usePathname();
-  console.log("xxx", pathname);
+  console.log('xxx', pathname);
 
   const onCloseNodalHandler = () => {
     setMobileMenuOpen(false);
   };
-
   return (
-    <header className="bg-white mb-5 lg:mb-0">
+    <header className='mb-5 bg-white lg:mb-0'>
       <nav
-        className="mx-auto flex max-w-7xl items-center justify-between pl-5 lg:px-8 lg:py-5"
-        aria-label="Global"
+        className='flex items-center justify-between pl-5 mx-auto max-w-7xl lg:px-8 lg:py-5'
+        aria-label='Global'
       >
-        <div className="flex lg:flex-1">
-          <a href="/" className="-m-1.5 p-1.5">
+        <div className='flex lg:flex-1'>
+          <a href='/' className='-m-1.5 p-1.5'>
             <SvgLogo />
           </a>
         </div>
-        <div className="flex lg:hidden">
+        <div className='flex lg:hidden'>
           <button
-            type="button"
-            className="-mt-2.5 relative inline-flex items-center justify-center rounded-md pt-2.5 text-gray-700"
+            type='button'
+            className='-mt-2.5 relative inline-flex items-center justify-center rounded-md pt-2.5 text-gray-700'
             onClick={() => setMobileMenuOpen(true)}
           >
             <SvgBurger />
           </button>
         </div>
-        <div className="hidden lg:flex lg:gap-x-12 text-gray-01 font-medium text-base">
+        <div className='hidden text-base font-medium lg:flex lg:gap-x-12 text-gray-01'>
           <Link
-            href="/"
+            href='/'
             className={classNames(
-              "w-[70px] hover:text-primary-01 hover:font-semibold scale-animation",
+              'w-[70px] hover:text-primary-01 hover:font-semibold transition-all duration-200',
               {
-                "text-primary-01 font-semibold": pathname === "/",
+                'text-primary-01 font-semibold': pathname === '/',
               }
             )}
           >
             Головна
           </Link>
           <Link
-            href="/catalog"
+            href='/catalog'
             className={classNames(
-              "w-[70px] hover:text-primary-01 hover:font-semibold scale-animation",
+              'w-[70px] hover:text-primary-01 hover:font-semibold transition-all duration-200',
               {
-                "text-primary-01 font-semibold": pathname.includes("/catalog"),
+                'text-primary-01 font-semibold': pathname.includes('/catalog'),
               }
             )}
           >
             Каталог
           </Link>
           <Link
-            href="/about-us"
+            href='/about-us'
             className={classNames(
-              "w-[70px] hover:text-primary-01 hover:font-semibold scale-animation",
+              'w-[70px] hover:text-primary-01 hover:font-semibold transition-all duration-200'    ,
               {
-                "text-primary-01 font-semibold": pathname === "/about-us",
+                'text-primary-01 font-semibold': pathname === '/about-us',
               }
             )}
           >
             Про нас
           </Link>
         </div>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end gap-4">
-          <button className="px-6 py-3 border-2 border-primary-01 rounded-[48px] scale-animation">
+        <div className='hidden gap-4 lg:flex lg:flex-1 lg:justify-end'>
+          <button className='px-6 py-3 border-2 border-primary-01 rounded-[48px] scale-animation'>
             <SvgCart />
           </button>
-          <button className="btn-primary scale-animation">
+          <button className='btn-primary scale-animation'>
             Особистий кабінет
           </button>
         </div>
       </nav>
       <Transition appear show={mobileMenuOpen} as={Fragment}>
-        <Dialog as="div" className="lg:hidden" onClose={onCloseNodalHandler}>
+        <Dialog as='div' className='lg:hidden' onClose={onCloseNodalHandler}>
           <Transition.Child
             as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
+            enter='ease-out duration-300'
+            enterFrom='opacity-0'
+            enterTo='opacity-100'
+            leave='ease-in duration-200'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
           >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
+            <div className='fixed inset-0 bg-black bg-opacity-25' />
           </Transition.Child>
           <Transition.Child
             as={Fragment}
-            enter="ease-out duration-200"
-            enterFrom="opacity-0 translate-x-1"
-            enterTo="opacity-100 translate-x-0"
-            leave="ease-out duration-200"
-            leaveFrom="opacity-100 translate-x-0"
-            leaveTo="opacity-0 translate-x-1"
+            enter='ease-out duration-200'
+            enterFrom='opacity-0 translate-x-1'
+            enterTo='opacity-100 translate-x-0'
+            leave='ease-out duration-200'
+            leaveFrom='opacity-100 translate-x-0'
+            leaveTo='opacity-0 translate-x-1'
           >
-            <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-primary-01 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-              <div className="pl-5 flex items-start justify-between">
+            <Dialog.Panel className='fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-primary-01 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10'>
+              <div className='flex items-start justify-between pl-5'>
                 <Link
-                  href="/"
-                  className="pt-3"
+                  href='/'
+                  className='pt-3'
                   onClick={() => setMobileMenuOpen(false)}
                 >
-                  <SvgCareLogo className="text-light-blue" />
+                  <SvgCareLogo className='text-light-blue' />
                 </Link>
                 <button
-                  type="button"
-                  className="-mt-2.5 rounded-md pt-2.5 text-gray-700"
+                  type='button'
+                  className='-mt-2.5 rounded-md pt-2.5 text-gray-700'
                   onClick={() => setMobileMenuOpen(false)}
                 >
                   <SvgBurger />
                 </button>
               </div>
-              <div className="mt-6 px-5 flow-root">
-                <div className="-my-6 divide-y divide-gray-500/10">
-                  <div className="space-y-2 pb-4 text-light-blue">
+              <div className='flow-root px-5 mt-6'>
+                <div className='-my-6 divide-y divide-gray-500/10'>
+                  <div className='pb-4 space-y-2 text-light-blue'>
                     <Link
-                      href="/"
+                      href='/'
                       onClick={() => setMobileMenuOpen(false)}
                       className={classNames(
-                        "block rounded-lg px-4 py-2 text-base font-semibold leading-7 scale-animation hover:bg-primary-02",
+                        'block rounded-lg px-4 py-2 text-base font-semibold leading-7 scale-animation hover:bg-primary-02',
                         {
-                          "bg-primary-02": pathname === "/",
+                          'bg-primary-02': pathname === '/',
                         }
                       )}
                     >
                       Головна
                     </Link>
                     <Link
-                      href="/catalog"
+                      href='/catalog'
                       onClick={() => setMobileMenuOpen(false)}
                       className={classNames(
-                        "block rounded-lg px-4 py-2 text-base font-semibold leading-7 scale-animation hover:bg-primary-02",
+                        'block rounded-lg px-4 py-2 text-base font-semibold leading-7 scale-animation hover:bg-primary-02',
                         {
-                          "bg-primary-02": pathname.includes("/catalog"),
+                          'bg-primary-02': pathname.includes('/catalog'),
                         }
                       )}
                     >
                       Каталог
                     </Link>
                     <Link
-                      href="/about-us"
+                      href='/about-us'
                       onClick={() => setMobileMenuOpen(false)}
                       className={classNames(
-                        "block rounded-lg px-4 py-2 text-base font-semibold leading-7 scale-animation hover:bg-primary-02",
+                        'block rounded-lg px-4 py-2 text-base font-semibold leading-7 scale-animation hover:bg-primary-02',
                         {
-                          "bg-primary-02": pathname === "/about-us",
+                          'bg-primary-02': pathname === '/about-us',
                         }
                       )}
                     >
                       Про нас
                     </Link>
                     <Link
-                      href="/about-us"
+                      href='/about-us'
                       onClick={() => setMobileMenuOpen(false)}
-                      className="block rounded-lg px-4 py-2 text-base font-semibold leading-7 scale-animation hover:bg-primary-02"
+                      className='block px-4 py-2 text-base font-semibold leading-7 rounded-lg scale-animation hover:bg-primary-02'
                     >
                       Корзина
                     </Link>
                     <Link
-                      href="/about-us"
+                      href='/about-us'
                       onClick={() => setMobileMenuOpen(false)}
-                      className="block rounded-lg px-4 py-2 text-base font-semibold leading-7 scale-animation hover:bg-primary-02"
+                      className='block px-4 py-2 text-base font-semibold leading-7 rounded-lg scale-animation hover:bg-primary-02'
                     >
                       Особистий кабінет
                     </Link>
--- src/app/(components)/Hero.tsx
+++ src/app/(components)/Hero.tsx
@@ -1,37 +1,42 @@
-import React from "react";
-import hero from "../../../public/images/hero.png";
-import heroMobile from "../../../public/images/hero-mobile.png";
-import Image from "next/image";
-import { SvgHeroArrow } from "../(svg)/AllSvg";
+import React from 'react';
+import hero from '../../../public/images/hero.png';
+import heroMobile from '../../../public/images/hero-mobile.png';
+import Image from 'next/image';
+import { SvgHeroArrow } from '../(svg)/AllSvg';
 
 const Hero = () => {
   return (
-    <div className="flex flex-col lg:flex-row px-7 lg:px-0 items-start justify-between lg:gap-8">
-      <div className="lg:min-w-[505px]">
-        <div className="hidden mt-6 lg:flex justify-end">
+    <div className='flex flex-col items-start justify-between lg:flex-row px-7 lg:px-0 lg:gap-8'>
+      <div className='lg:min-w-[505px]'>
+        <div className='justify-end hidden mt-6 lg:flex'>
           <SvgHeroArrow />
         </div>
-        <div className="-mt-10 space-y-5">
-          <div className="font-semibold text-4xl sm:text-[50px]">
-            Оренда речей <span className="text-primary-01">LendoCare</span>
+        <div className='-mt-10 space-y-5'>
+          <div className='sm:space-y-4'>
+            <div className='font-semibold text-4xl sm:text-[50px]'>
+              Оренда речей
+            </div>
+            <div className='font-semibold text-4xl sm:text-[50px] text-primary-01'>
+              LendoCare
+            </div>
           </div>
-          <div className="text-lg font-medium text-gray-01">
+          <div className='text-lg font-medium text-gray-01'>
             Оренда обладнання для людей в інвалідністю без жодних проблем.
             Забезпечення доступності медичного обладнання та технологій для
             використання вдома через оренду.
           </div>
-          <div className="pt-4 space-x-6">
-            <button className="btn-primary">Каталог</button>
-            <button className="btn-outline">Детальніше</button>
+          <div className='pt-4 space-x-6'>
+            <button className='btn-primary'>Каталог</button>
+            <button className='btn-outline'>Детальніше</button>
           </div>
         </div>
       </div>
-      <div className="hidden lg:block my-auto">
-        <Image src={hero} alt="hero" />
+      <div className='hidden my-auto lg:block'>
+        <Image src={hero} alt='hero' />
       </div>
-      <div className="w-full flex lg:hidden justify-end">
-        <div className="h-[550px] w-[550px]">
-          <Image src={heroMobile} alt="hero" />
+      <div className='flex justify-end w-full lg:hidden'>
+        <div className='h-[550px] w-[550px]'>
+          <Image src={heroMobile} alt='hero' />
         </div>
       </div>
     </div>
