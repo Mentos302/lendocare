e550364901a40471599ab72174cb3de25866973c
Ihor Melnyk <73445190+Mentos302@users.noreply.github.com>
2023-06-22 20:53:09 +0300

    add new pages

---
+++ src/app/checkout/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+
+const CheckoutPage = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data: any) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <fieldset>
+        <b>Контактна інформація</b>
+
+        <input
+          type="phone"
+          placeholder="Ваше ім'я"
+          {...register("name", { required: true })}
+        />
+        <input
+          type="phone"
+          placeholder="+380 93 810 52 39"
+          {...register("phone", { required: true })}
+        />
+      </fieldset>
+      <fieldset>
+        <b>Інформація для доставки</b>
+
+        <input
+          type="radio"
+          {...register("delivery_type", { required: true })}
+        />
+
+        <input
+          type="phone"
+          placeholder="Ваше ім'я"
+          {...register("name", { required: true })}
+        />
+        <input
+          type="phone"
+          placeholder="+380 93 810 52 39"
+          {...register("phone", { required: true })}
+        />
+      </fieldset>
+    </form>
+  );
+};
+
+export default CheckoutPage;
--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -4,21 +4,21 @@ import { Product } from "@/app/types";
 import apiClient from "@/utils/http-common";
 import { useQuery } from "@tanstack/react-query";
 
-type propTypes = { params: { slug: string } };
+type propTypes = { params: { id: number } };
 
-async function getProduct(slug: string) {
+async function getProduct(id: number) {
   const res = await apiClient.post("enquiry", {
-    lendoProductNames: [slug],
+    lendoProductIds: [id],
     countryCode: process.env.NEXT_PUBLIC_COUNTRY_CODE,
   });
 
-  return res.data;
+  return res.data.length ? res.data[0] : null;
 }
 
 const ProductPage = ({ params }: propTypes) => {
-  const { data, isLoading, error } = useQuery<Product>(
-    ["product", params.slug],
-    () => getProduct(params.slug)
+  const { data, isLoading, error } = useQuery<Product[]>(
+    ["product", params.id],
+    () => getProduct(params.id)
   );
 
   if (isLoading) return "Loading...";
@@ -27,8 +27,8 @@ const ProductPage = ({ params }: propTypes) => {
   if (data)
     return (
       <div>
-        <h1>{params.slug}</h1>
-        <h1>{data?.lendoProduct.name}</h1>
+        <h1>{params.id}</h1>
+        <h1>{data[0].lendoProduct.name}</h1>
       </div>
     );
 
